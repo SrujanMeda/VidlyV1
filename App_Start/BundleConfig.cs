1ee25732b6ccf67e381ac139b8d29e09fdca0828
SrujanMeda <56676140+SrujanMeda@users.noreply.github.com>
2025-06-06 16:07:20 +0530

    Add a bootstrap theme.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace VidlyV1
 {
@@ -23,7 +22,7 @@ namespace VidlyV1
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
