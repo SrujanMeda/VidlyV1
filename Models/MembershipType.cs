1f62abdb75d42937fef96479ee4c39089e039062
SrujanMeda <56676140+SrujanMeda@users.noreply.github.com>
2025-06-06 18:38:21 +0530

    Display membership type in the list of customers

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -1,8 +1,12 @@
-﻿namespace VidlyV1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidlyV1.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
